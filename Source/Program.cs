dc86965bf3da008b7e4c45b261cc2c63069d99bf
konaOnTheHub <nagy.petike2@gmail.com>
2025-04-13 23:20:53 +0100

    Added a print method to print founditems and lostitems

--- Source/Program.cs
+++ Source/Program.cs
@@ -212,4 +212,47 @@ class Program
         Console.WriteLine("--------------------------------------------\nFound item created successfully.\n--------------------------------------------");
         return;
     }
+    //Method to print a table of items
+    //Made it modular using c# reflection so that it can be used for an object of any class
+    static void printTable<T>(List<T> itemsToPrint)
+    {
+        var type = typeof(T);
+        //Get the properties of the type
+        var properties = type.GetProperties();
+        var columnWidth = new Dictionary<string, int>();
+
+        //Calculate the width of each column based on the property name and value length
+        foreach (var property in properties)
+        {
+            var maxLength = property.Name.Length;
+            foreach (var item in itemsToPrint)
+            {
+                var value = property.GetValue(item)?.ToString() ?? string.Empty;
+                if (value.Length > maxLength)
+                {
+                    maxLength = value.Length;
+                }
+            }
+            columnWidth[property.Name] = maxLength;
+        }
+
+        // Build dynamic format string
+        string format = "| " + string.Join(" | ", properties.Select(p => $"{{0,-{columnWidth[p.Name]}}}")) + " |";
+
+        // Print header
+        var headers = properties.Select(p => p.Name).ToArray();
+        Console.WriteLine(format, headers);
+
+        // Print separator
+        int totalWidth = columnWidth.Values.Sum() + (3 * properties.Length) + 1;
+        Console.WriteLine(new string('-', totalWidth));
+
+        // Print rows
+        foreach (var item in itemsToPrint)
+        {
+            var values = properties.Select(p => (p.GetValue(item)?.ToString()) ?? "").ToArray();
+            Console.WriteLine(format, values);
+        }
+
+    }
 }
