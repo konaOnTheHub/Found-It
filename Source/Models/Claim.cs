73a7cb19a3d2e7f13c1e27c030fcbac4439395f6
rayan2551 <121576089+rayan2551@users.noreply.github.com>
2025-04-13 10:38:43 +0100

    Updated Claim.cs

--- Source/Models/Claim.cs
+++ Source/Models/Claim.cs
@@ -11,18 +11,13 @@ namespace Source.Models
     {
         [Key]
         public int ClaimId { get; set; } // Primary key
-
         [ForeignKey("User")]
         public int UserId { get; set; } // Foreign key to User
         public User User { get; set; } // Navigation property to User
-
         [ForeignKey("FoundItem")]
         public int FoundItemId { get; set; } // Foreign key to FoundItem
         public FoundItem FoundItem { get; set; } // Navigation property to FoundItem
-
         public DateTime DateClaimed { get; set; } // Date when the claim was made
-      
-        [MaxLength(50)]
         public string Status { get; set; } = string.Empty; // Status of the claim 
     }
 }
